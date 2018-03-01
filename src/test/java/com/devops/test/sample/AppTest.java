fc4b833036d55ff18ca34753874ca938a335b87d
Chandrasekhar <raoch88@gmail.com>
2018-03-01 09:48:36 -0500

    initial commit

---
+++ src/test/java/com/devops/test/sample/AppTest.java
@@ -0,0 +1,45 @@
+package com.devops.test.sample;
+
+
+
+import org.apache.log4j.Logger;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+	private Logger logger = Logger.getLogger(AppTest.class);
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+       // logger.info("sample devops test");
+        System.out.println("Sample devops test >>>>>>>");
+    }
+}
